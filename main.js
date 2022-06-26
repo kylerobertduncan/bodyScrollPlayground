c35a355fa96cc53e2259f2308c29fc1d9ceed7dd
Kyle <kylerobertduncan@gmail.com>
2022-06-26 12:05:09 -0400

    Try shrink map on scroll

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 document.getElementById("toTop").addEventListener("click", () => {
-  // const mapHeight = document.querySelector(".mapSpace").offsetHeight;
-  // console.log("map height", mapHeight);
-  // const article = document.querySelector(".article");
-  // console.log(article);
-
-  // document.body.scrollTop = 0;
-  // document.documentElement.scrollTop = 0;
-
   window.scroll({
-    top: 0,
-    // left: 0,
-    behaviour: "smooth"
+    behaviour: 'smooth',
+    top: 0
   });
 });
+
+function shrinkMap() {
+  if (document.body.scrollTop > 150) {
+    const map = document.querySelector(".map");
+    const diff = document.body.scrollTop - 150;
+    map.style.height = `calc(50% - ${diff})`;
+  } 
+}
+window.onscroll = () => shrinkMap();
